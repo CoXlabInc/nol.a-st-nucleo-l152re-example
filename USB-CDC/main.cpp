fd0dc644af88c9a660bfe7864d8755b667f82220
Jongsoo Jeong <jsjeong@coxlab.kr>
2016-08-22 13:53:54 +0900

    CS-149 Added VCP example.

--- USB-CDC/main.cpp
+++ USB-CDC/main.cpp
@@ -1,5 +1,51 @@
 #include <cox.h>
 
-void setup(void)
-{
+Timer tPrint;
+SerialPort *vcp = NULL;
+
+static void printTask(void *) {
+  System.ledToggle();
+  printf("[%lu usec] Hi!\n", micros());
+  vcp->printf("[%lu usec] Hi!\n", micros());
+}
+
+static void keyboard(SerialPort&) {
+  System.ledToggle();
+
+  while (Serial.available() > 0) {
+    printf("[%lu usec] Keyboard input: 0x%02X\n", micros(), Serial.read());
+  }
+}
+
+static void button() {
+  System.ledToggle();
+  printf("[%lu usec] User button pressed\n", micros());
+}
+
+static void eventVcpInput(SerialPort &) {
+  System.ledToggle();
+
+  while (vcp->available() > 0) {
+    vcp->printf("[%lu usec] Keyboard input: 0x%02X\n", micros(), vcp->read());
+  }
+}
+
+void setup() {
+  System.ledOn();
+  Serial.begin(115200);
+  printf("\n*** Serial test for ST Nucleo-L152RE ***\n");
+
+  pinMode(BUTTON_BUILTIN, INPUT);
+  attachInterrupt(BUTTON_BUILTIN, button, FALLING);
+
+  tPrint.onFired(printTask, NULL);
+  tPrint.startPeriodic(1000);
+
+  Serial.onReceive(keyboard);
+  Serial.listen();
+
+  vcp = System.enableVCP();
+  vcp->begin();
+  vcp->onReceive(eventVcpInput);
+  vcp->listen();
 }
